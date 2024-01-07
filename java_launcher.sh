b0e219771d3d006f7474076b23b33f0ab8089dfb
overwatcheddude <overwatcheddude@gmail.com>
2024-01-07 07:16:47 +0400

    Added .jar filter to file selector

--- java_launcher.sh
+++ java_launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export PATH=/app/jre/bin:/usr/bin
 
-jar_file=`zenity --file-selection --title="Select a File"`
+jar_file=`zenity --file-selection --file-filter='Java Archive files | *.jar'`
 
 java -jar "$jar_file"
